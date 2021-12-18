53428b3376e91c8622dfed023d47ec9f17687213
Sanjith <85774398+Sanjith7146@users.noreply.github.com>
2021-12-18 21:48:30 +0800

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by sanjithsethu on 18/12/21.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
